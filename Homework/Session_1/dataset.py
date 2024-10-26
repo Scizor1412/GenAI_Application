51a38c6acb6070ee3faaaaad7035a56e63474608
Nguyen <nguyenlehoang212@gmail.com>
2024-10-26 21:13:34 +0700

    remove duplicates

--- Homework/Session_1/dataset.py
+++
@@ -1,9 +0,0 @@
-#  `input_datas` và `labels`.
-
-class Dataset:
-    def __init__(self, input_datas, labels):
-        self.input_datas = input_datas
-        self.labels = labels
-        print(input_datas)
-        print(labels)
-        
